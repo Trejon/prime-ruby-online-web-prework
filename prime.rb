8f27e9e0604c9c02d910465041f676482f0477a3
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-07 16:37:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 # Add  code here!
 def prime?(num)
   return false if num <= 1 
-  new_array = 2..200
+  new_array = 2..(num ** 0.5)
   new_array.each do |i|
-  return true if num % i == 0 
+  return false if num % i == 0 
   end
+  true
 end
